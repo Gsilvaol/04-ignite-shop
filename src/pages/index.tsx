cffbc32aab6427f6585648b70616cb19cced22c9
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-05-21 15:18:44 -0300

    Estrutura da página de produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Link href={`/product/${product.id}`} key={product.id}>
+          <Link legacyBehavior href={`/product/${product.id}`} key={product.id}>
           <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt="" />
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,26 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>
-            Product: {JSON.stringify(query)}
-        </h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta x</h1>
+                <span>R$ 79,90</span>
+                <p>Esta camiseta é a definição de estilo despojado e moderno. Cada peça é única graças ao processo artesanal de tingimento. Feita de algodão 100% orgânico, esta camiseta não só é confortável e respirável, mas também sustentável.
+
+                    O design oversized proporciona um ajuste solto e casual, perfeito para qualquer ocasião, seja um passeio no parque ou um encontro com amigos. Combine-a com jeans rasgados para um visual streetwear ou com shorts para um look mais descontraído.
+
+                    Com acabamento de alta qualidade e costuras reforçadas, esta camiseta é feita para durar e ser uma peça-chave no seu guarda-roupa. Seja a estrela do seu próprio desfile de moda com esta camiseta que une conforto, estilo e sustentabilidade em uma só peça.</p>
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
