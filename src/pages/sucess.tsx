6e53b84fabeade7ce7d8ff94daf44b7ad5d95312
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-05-27 15:09:26 -0300

    Estrutura de sucesso

--- src/pages/sucess.tsx
+++ src/pages/sucess.tsx
@@ -1,7 +1,22 @@
+import { ImageContainer, SucessContainer } from "@/styles/pages/sucess";
+import Link from "next/link";
+
 export default function Sucess() {
     return (
-        <h1>
-            Sucess
-        </h1>
+        <SucessContainer>
+            <h1>Compra Efetuada</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuul <strong>Gustavo</strong>, sua <strong>Camiseta</strong> já está a caminho.
+            </p>
+
+            <Link href="/">
+                Voltar ao Catálogo
+            </Link>
+        </SucessContainer>
     )
 }
